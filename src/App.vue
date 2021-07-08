397263a1b94accbfcb942b27a042ebd1ff9ff827
hashkapil <hashkapilkalra@gmail.com>
2021-07-08 17:10:37 +0530

    vuejs setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <loader-component v-if="displayLoader"/>
+
+        <notifications group="notify" class="notify"/>
+
+        <router-view/>
+    </div>
+</template>
+
+<script>
+import loaderComponent from '@/components/LoaderComponent'
+import { mapGetters } from 'vuex'
+
+export default {
+    name: 'App',
+    components: {
+        loaderComponent
+    },
+    computed: {
+        ...mapGetters({
+            displayLoader: 'getDisplayLoader'
+        })
+    },
+    metaInfo () {
+        return {
+            title: this.$appName,
+            titleTemplate: `%s - ` + this.$appName
+        }
+    },
+}
+</script>
