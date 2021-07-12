bfa01b3a8a1cb3c6980019c0baf7cc2b86bd0bf6
Mark <hashsourabh@gmail.com>
2021-07-12 15:51:00 +0530

    login page design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Login</router-link> |
-    <router-link to="/register">Register</router-link>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
-@import "nav.scss";
+	@import "assets/scss/app.scss";
 </style>
