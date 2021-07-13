18579c06eb49abd0adefd02f8f861b6f2cc3763a
ricki <hashkapilkalra@gmail.com>
2021-07-13 11:36:17 +0530

    create home page

--- src/main.js
+++ src/main.js
@@ -1,24 +1,25 @@
-import Vue from 'vue';
+window.Vue = require('vue');
+// import Vue from 'vue';
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Vuelidate from 'vuelidate';
-Vue.use(Vuelidate)
-import axios from "axios";
-// const axios = require('axios').default;
+// import Vuelidate from 'vuelidate';
+// Vue.use(Vuelidate)
+// import axios from "axios";
+// // const axios = require('axios').default;
 
-axios.interceptors.request.use(
-    config => {
-      const token = localStorage.getItem("access_token");
-      if (token) {
-        config.headers.common["Authorization"] = 'Bearer ' + token;
-      }
-      return config;
-    },
-    error => {
-      return Promise.reject(error);
-    }
-  );
+// axios.interceptors.request.use(
+//     config => {
+//       const token = localStorage.getItem("access_token");
+//       if (token) {
+//         config.headers.common["Authorization"] = 'Bearer ' + token;
+//       }
+//       return config;
+//     },
+//     error => {
+//       return Promise.reject(error);
+//     }
+//   );
 
 createApp(App).use(store).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="home">
+        This home homepage
+    </div>
+</template>
